3fbd73d0f077cfd1f7ff64a039dcf3252d6cb575
AdameK <153874504+AdameKlive@users.noreply.github.com>
2025-11-26 08:09:09 +0100

    Fix link case sensitivity for Inkoguto

--- creators.js
+++ creators.js
@@ -1,10 +1,11 @@
 const creators = [
   { name: "Slayproxx", img: "img/tworca6.png", link: "slayproxx.html" },
-  { name: "Inkoguto", img: "img/tworca7.png", link: "Inkoguto.html" },
+  { name: "Inkoguto", img: "img/tworca7.png", link: "inkoguto.html" },
   { name: "Boruciak", img: "img/tworca2.png", link: "boruciak.html" },
   { name: "Abra Games", img: "img/tworca3.png", link: "abragames.html" },
   { name: "Abra plus", img: "img/tworca4.png", link: "abraplus.html" },
   { name: "SharQ", img: "img/tworca5.png", link: "sharq.html" },
   { name: "Narszociak", img: "img/tworca1.png", link: "narszociak.html" },
 ];
+
 
